b2bf7efe749da58872e148812a679b9ce60144f5
gabyslaw <gabyslaw@gmail.com>
2020-11-21 19:30:29 +0100

    Added Products Repository

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            var productBrand = await _context.ProductBrands.ToListAsync();
+
+            return productBrand;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            var product = await _context.Products
+            .Include(p => p.ProductBrand)
+            .Include(p => p.ProductType)
+            .FirstOrDefaultAsync(p => p.Id == id);
+           
+            return product;
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            var products = await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .ToListAsync();
+
+            return products;
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            var productType = await _context.ProductTypes.ToListAsync();
+
+            return productType;
+        }
+    }
+}
