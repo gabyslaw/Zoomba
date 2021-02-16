ed3bdf257f4130856b089eb16b020c04daabed04
gabyslaw <gabyslaw@gmail.com>
2021-02-16 12:03:53 +0100

    Added CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,6 +64,14 @@ namespace API
 
             services.AddSwaggerDocumentation();
 
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,6 +86,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
